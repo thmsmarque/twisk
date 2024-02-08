a1a568f245b6b4a5044b0ce8684be982e136c597
FONTAINE Clara <clara.fontaine3@etu.univ-lorraine.fr>
2024-02-08 10:47:08 +0100

    Debut etape

---
+++ src/main/java/Etape.java
@@ -0,0 +1,28 @@
+package main.java;
+
+import java.lang.String;
+import java.util.Iterator;
+
+public abstract class Etape {
+    private String nom;
+
+    public Etape(String nom){
+        this.nom = nom;
+    }
+
+    void AjouterSuccesseur(Etape...successeur){
+
+    }
+
+    boolean estUneActivite(){
+    return false;
+    }
+
+    boolean estUnGuichet(){
+    return false;
+    }
+
+    Iterator<Etape> iterator(){
+    return this.iterator();
+    }
+}
