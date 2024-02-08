71744f8144eae0902483f951f873b4c73e48799f
thmsmarque <thmsmarque@gmail.com>
2024-02-08 11:04:13 +0100

    Ajout des commentaires

--- src/main/java/GestionnaireEtapes.java
+++ src/main/java/GestionnaireEtapes.java
@@ -1,4 +1,47 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class GestionnaireEtapes {
+
+    ArrayList<Etape> etapes;
+
+    /**
+     * Constructeur de la classe GestionnaireEtapes
+     *
+     */
+    public GestionnaireEtapes()
+    {
+
+    }
+
+    /**
+     * Méthode pour ajouter des étapes au gestionnaire
+     *
+     * @param etapes les étapes à ajouter
+     */
+    public void ajouter(Etape... etapes)
+    {
+
+    }
+
+    /**
+     * Renvoi un entier correspondant au nombre d'étapes du gestionnaire
+     * @return int nombre d'étapes du gestionnaire
+     */
+    public int nbEtapes()
+    {
+        return etapes.size();
+    }
+
+    /**
+     * Crée l'iterator de la classe
+     * @return iterator
+     */
+    Iterator<Etape> iterator()
+    {
+        return etapes.iterator();
+    }
+
 }
--- src/main/java/Monde.java
+++ src/main/java/Monde.java
@@ -1,4 +1,72 @@
 package main.java;
 
+import java.util.Iterator;
+
 public class Monde {
+    GestionnaireEtapes lesEtapes;
+    SasEntree entree;
+    SasSortie sortie;
+
+    /**
+     * Constructeur de la classe Monde
+     */
+    public Monde()
+    {
+
+    }
+
+    /**
+     * Méthode qui définit les entrées
+     * @param etapes etapes à définir comme entrées
+     */
+    public void aCommeEntree(Etape... etapes)
+    {
+
+    }
+
+    /**
+     * Méthode qui définit les sorties
+     *
+     * @param etapes étapes à définir comme sorties
+     */
+    public void aCommeSortie(Etape... etapes)
+    {
+
+    }
+
+    /**
+     * Méthode pour ajouter des étapes au Monde
+     * @param etapes étapes à ajouter au Monde
+     */
+    public void ajouter(Etape... etapes)
+    {
+
+    }
+
+    /**
+     * Méthode qui renvoi le nombre d'étapes du monde
+     * @return nombre d'étapes du monde
+     */
+    public int nbEtapes()
+    {
+        return 0;
+    }
+
+    /**
+     * Méthode qui renvoi le nombre de guichets du monde
+     * @return nombre de guichets du monde
+     */
+    public int nbGuichets()
+    {
+        return 0;
+    }
+
+    /**
+     * Crée l'iterator de la classe monde
+     * @return l'iterator de la classe monde
+     */
+    public Iterator<Etape> iterator()
+    {
+        return lesEtapes.iterator();
+    }
 }
