87a5865bc9eceb95d9538c52f31025d7181b9eb3
thmsmarque <thmsmarque@gmail.com>
2024-03-12 13:26:07 +0100

    Nouveau def.h

--- src/main/ressources/codeC/def.h
+++ src/main/ressources/codeC/def.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-// fonctions déclarées dans programmeC, à utiliser dans le code C du client ******************************************************
+/* fonctions déclarées dans programmeC, à utiliser dans le code C du client ******************************************************/
 
 void delai(int temps, int delta) ;
 
@@ -15,13 +15,13 @@ void transfert(int source, int destination) ;
 int P(int semid, int numero) ;
 int V(int semid, int numero) ;
 
-// fonctions déclarées dans programmeC, à utiliser dans le code java ************************************************************
+/* fonctions déclarées dans programmeC, à utiliser dans le code java ************************************************************/
 
 int* start_simulation(int nbEtapes, int nbGuichets, int nbClients, int *tabJetonsGuichets) ;
 int* ou_sont_les_clients(int nbEtapes, int nbClients) ;
 void nettoyage() ;
 
-// la fonction déclarée dans le code C des clients ******************************************************************************
+/* la fonction déclarée dans le code C des clients ******************************************************************************/
 
 void simulation(int ids) ;
 
