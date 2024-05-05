6366d5f3de410570fe8111c7fd3fe02cddc7e838
fontain58u <clara.fontaine3@etu.univ-lorraine.fr>
2024-05-05 17:11:04 +0200

    Début bifurcation (des erreurs à modifier)

--- src/main/java/depot/ClientMonde.java
+++ src/main/java/depot/ClientMonde.java
@@ -9,13 +9,19 @@ public class ClientMonde {
         Monde monde = new Monde();
 
         Activite zoo = new Activite("balade au zoo", 3, 1);
+        Activite zoo2 = new Activite("balade", 3, 1);
+
         Guichet guichet = new Guichet("accès au toboggan", 2);
         Activite tob = new ActiviteRestreinte("toboggan", 2, 1);
 
-        zoo.ajouterSuccesseur(guichet);
+       //zoo.ajouterSuccesseur(guichet);
+       zoo.ajouterSuccesseur(guichet,zoo2);
+       zoo2.ajouterSuccesseur(tob);
+
         guichet.ajouterSuccesseur(tob);
 
-        monde.ajouter(zoo, tob, guichet);
+        //monde.ajouter(zoo, tob, guichet);
+        monde.ajouter(zoo,zoo2, tob, guichet);
 
         monde.aCommeEntree(zoo);
         monde.aCommeSortie(tob);
--- src/main/java/twisk/monde/Activite.java
+++ src/main/java/twisk/monde/Activite.java
@@ -1,6 +1,7 @@
 package twisk.monde;
 
 import twisk.outils.FabriqueNumero;
+import twiskIG.mondeIG.EtapeIG;
 
 import java.util.Iterator;
 
@@ -67,6 +68,60 @@ public class Activite extends Etape {
     }
 
     public String toC() {
+        int nbSuccesseur = this.gestionnaire.nbEtapes();
+
+        System.out.println("Nombre d'étape de "+ getNom() + " " + nbSuccesseur);
+
+
+        Iterator<Etape> it = this.getGestionnaire().iterator();
+        Etape et = it.next();
+        String res = "";
+        if(nbSuccesseur==1)
+        {
+            //Dans le cas où l'étape actuelle n'a qu'un seul successeur
+            res = "delai("+this.temps+","+this.ecartTemps+");\n" +
+                    "transfert("+this.getDefineName()+","+et.getDefineName()+");\n" + et.toC();
+        }else if(nbSuccesseur>1)
+        {
+            //Dans le cas où l'étape actuelle a plusieurs successeurs
+            res = "int nb = (int) ((rand() / (float) RAND_MAX)*" + this.getGestionnaire().nbEtapes() + ");\n" +
+                    "switch(nb){\n";
+            StringBuilder build = new StringBuilder(res);
+
+            int i = 0;
+
+            for(Etape e : gestionnaire){
+                String suivant = e.toC();
+                String actuel = "delai("+this.temps+","+this.ecartTemps+");\n" +
+                        "transfert("+this.getDefineName()+","+et.getDefineName()+");\n" ;
+                if(i==0) {
+                    build.append("\n if (nb==").append(i).append(") { \n").append(actuel).append(" ").append(suivant).append("\n");
+                } else {
+                    build.append("else if (nb ==").append(i).append(") { \n").append(actuel).append(" ").append(suivant).append("\n } \n");
+                }
+                i++;
+
+            }
+            /*
+            while(it.hasNext())
+            {
+                build.append("\tcase " + i +":" + et.toC() + "break;\n");
+                i++;
+                et = it.next();
+            }
+            build.append("perror(\"Erreur lors de la création\"); \n" +
+                    "break;" +
+                    "} \n");
+            res = build.toString();
+*/
+            res = build.toString();
+        }
+
+        return res;
+    }
+
+    /*
+    public String toC() {
         Iterator<Etape> it = this.getGestionnaire().iterator();
         Etape et = it.next();
         String res;
@@ -98,7 +153,7 @@ public class Activite extends Etape {
 
         return res;
     }
-
+*/
 
     @Override
     public String defineName() {
