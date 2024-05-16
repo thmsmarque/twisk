229b32f71597f4d9e0d07df0d6070a7a9d2ed425
fontain58u <clara.fontaine3@etu.univ-lorraine.fr>
2024-05-16 14:01:01 +0200

    ON détecte les cycles !!!!

--- src/main/java/twiskIG/exceptions/TwiskException.java
+++ src/main/java/twiskIG/exceptions/TwiskException.java
@@ -6,13 +6,6 @@ public class TwiskException extends Exception {
 
     public TwiskException(String exception) {
         super(exception);
-        // Créer une boîte de dialogue d'information
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Erreur!");
-        alert.setHeaderText(null); // Pas de texte d'en-tête
-        alert.setContentText(exception);
-        // Afficher la boîte de dialogue
-        alert.showAndWait();
     }
 
 }
