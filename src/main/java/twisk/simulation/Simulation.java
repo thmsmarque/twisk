75ef98f515cabf58e5cf3fc74148482ce31ed0e5
fontain58u <clara.fontaine3@etu.univ-lorraine.fr>
2024-05-31 14:18:36 +0200

    Ajout du twiiiiiist

--- src/main/java/twisk/simulation/Simulation.java
+++ src/main/java/twisk/simulation/Simulation.java
@@ -40,128 +40,130 @@ public class Simulation extends SujetObserve {
         System.out.println(this.getListeobs().toString());
         mondeIG.switchEtatSim();
         setNbClients(mondeIG.getNbCLient());
+
+        long debutSim = System.nanoTime();
+
         System.out.println("Lancement de la simulation logique");
-                GestionnaireClients g = monde.getG();
-                KitC kit = new KitC();
-                monde.probaType(mondeIG.getProba());
-                String fichierC = monde.toC();
-                System.out.println(fichierC);
-                kit.creerFichier(fichierC);
-                kit.compiler();
-                kit.construireLaBibliothese();
+        GestionnaireClients g = monde.getG();
+        KitC kit = new KitC();
+        monde.probaType(mondeIG.getProba());
+        String fichierC = monde.toC();
+        System.out.println(fichierC);
+        kit.creerFichier(fichierC);
+        kit.compiler();
+        kit.construireLaBibliothese();
 
-                //Chargement de la bibliothèque où sont définies les fonctions
-                System.load("/tmp/twisk/libTwisk"+ FabriqueNumeroLibTwisk.getInstance().getNumero()+".so") ;
-                FabriqueNumeroLibTwisk.getInstance().incrementer();
+        //Chargement de la bibliothèque où sont définies les fonctions
+        System.load("/tmp/twisk/libTwisk" + FabriqueNumeroLibTwisk.getInstance().getNumero() + ".so");
+        FabriqueNumeroLibTwisk.getInstance().incrementer();
 
-                // Ecriture du main.c ---------------------------------
-                int nbEtapes = monde.nbEtapes();
-                int nbGuichets = monde.nbGuichets();
-                int[] tabJetonsGuichet = new int[nbGuichets];
-                for(Etape et : monde.getLesEtapes())
-                {
-                    if(et.estUnGuichet())
-                    {
-                        Guichet guich = (Guichet)et;
-                        tabJetonsGuichet[guich.getSemaphore()-1] = guich.getNbJetons();
-                    }
-                }
+        // Ecriture du main.c ---------------------------------
+        int nbEtapes = monde.nbEtapes();
+        int nbGuichets = monde.nbGuichets();
+        int[] tabJetonsGuichet = new int[nbGuichets];
+        for (Etape et : monde.getLesEtapes()) {
+            if (et.estUnGuichet()) {
+                Guichet guich = (Guichet) et;
+                tabJetonsGuichet[guich.getSemaphore() - 1] = guich.getNbJetons();
+            }
+        }
 
-                for(int i=0; i<tabJetonsGuichet.length;i++)
-                {
-                    System.out.print((i+1) + " : "+tabJetonsGuichet[i]+ "\t");
-                }
-                //lancement simulation : -----------------------------
-                int[] resSim;
-                resSim = start_simulation(nbEtapes,nbGuichets,nbClients,tabJetonsGuichet);
-                //Gestionnaire de clients :
-                g.setClients(resSim);
+        for (int i = 0; i < tabJetonsGuichet.length; i++) {
+            System.out.print((i + 1) + " : " + tabJetonsGuichet[i] + "\t");
+        }
+        //lancement simulation : -----------------------------
+        int[] resSim;
+        resSim = start_simulation(nbEtapes, nbGuichets, nbClients, tabJetonsGuichet);
+        //Gestionnaire de clients :
+        g.setClients(resSim);
 
 
-                //affichage des numéros de processus :
-                System.out.println("Les clients :");
-                for(int i=0; i<nbClients;i++){
-                    System.out.print(resSim[i]+ " ");
-                }
-                System.out.println("\n ");
+        //affichage des numéros de processus :
+        System.out.println("Les clients :");
+        for (int i = 0; i < nbClients; i++) {
+            System.out.print(resSim[i] + " ");
+        }
+        System.out.println("\n ");
 
-                int[] posClients = ou_sont_les_clients(nbEtapes,nbClients);
+        int[] posClients = ou_sont_les_clients(nbEtapes, nbClients);
 
-                //On affiche où sont les clients :
-                do {
-                    posClients = ou_sont_les_clients(nbEtapes,nbClients);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    int etapeActuel = 0;
-                    for(int i =0;i<nbEtapes;i++) {
-                        if (etapeActuel != monde.getSortie().getIndiceEtape()) {
-                            System.out.print("Etape " + monde.getLesEtapes().getListeetapes().get(i).getNom() + " | Nombre de personnes : " + posClients[i + nbClients * i] + "  => ");
-
-
-
-                            int nbClientDansAct = posClients[i + nbClients * i];
-                            int rang = 0;
-                            for (int j = i + nbClients * i + 1; j < i + nbClients * i + 1 + nbClientDansAct; j++) {
-                                //j est initié à la position dans le tableau posClients où se trouve le premier client de l'étape
-                                //S'il n'y a aucun client dans l'étape alors j >= à la condition qui sort de la boucle
-                                System.out.print(posClients[j]+ " ");
+        //On affiche où sont les clients :
+        do {
+            posClients = ou_sont_les_clients(nbEtapes, nbClients);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            int etapeActuel = 0;
+            for (int i = 0; i < nbEtapes; i++) {
+                if (etapeActuel != monde.getSortie().getIndiceEtape()) {
+                    System.out.print("Etape " + monde.getLesEtapes().getListeetapes().get(i).getNom() + " | Nombre de personnes : " + posClients[i + nbClients * i] + "  => ");
 
 
-                                //On met à jour le gestionnaire de clients :
-                                g.allerA(posClients[j],monde.getLesEtapes().getListeetapes().get(i),rang);
-                                rang++;
-                            }
-                            System.out.println(" ");
-                        }
-                        etapeActuel++;
-                    }
-
-
-                    System.out.print("Etape Sortie client(s) : " + " | Nombre de personnes : " + posClients[monde.getSortie().getIndiceEtape()*nbClients+1] + " => ");
-                    int nbClientDansAct = posClients[monde.getSortie().getIndiceEtape()*nbClients+1];
-
-
-
-                    //System.out.println("Nb clients act" + nbClientDansAct);
-                    //System.out.println("Boucle : " + nbClients+2+nbClientDansAct);
-                    for(int j = nbClients+2; j<nbClients+2+nbClientDansAct;j++){
-
+                    int nbClientDansAct = posClients[i + nbClients * i];
+                    int rang = 0;
+                    for (int j = i + nbClients * i + 1; j < i + nbClients * i + 1 + nbClientDansAct; j++) {
                         //j est initié à la position dans le tableau posClients où se trouve le premier client de l'étape
                         //S'il n'y a aucun client dans l'étape alors j >= à la condition qui sort de la boucle
                         System.out.print(posClients[j] + " ");
-                        g.allerA(posClients[j],monde.getSortie(),monde.getSortie().getIndiceEtape());
 
+
+                        //On met à jour le gestionnaire de clients :
+                        g.allerA(posClients[j], monde.getLesEtapes().getListeetapes().get(i), rang);
+                        rang++;
                     }
                     System.out.println(" ");
-                    etapeActuel++;
-                    System.out.println("\n");
-                    notifierObservateurs();
-                    mondeIG.notifierObservateurs();
-                }while(posClients[monde.getSortie().getIndiceEtape()*nbClients+1] != nbClients && mondeIG.getEnCoursDeSim()); //while tous les clients ne sont pas dans le sasSortie donc posClient[nbAct-1 + nbClient * nbAct-1] == nbClient
+                }
+                etapeActuel++;
+            }
+
+
+            System.out.print("Etape Sortie client(s) : " + " | Nombre de personnes : " + posClients[monde.getSortie().getIndiceEtape() * nbClients + 1] + " => ");
+            int nbClientDansAct = posClients[monde.getSortie().getIndiceEtape() * nbClients + 1];
+
+
+            //System.out.println("Nb clients act" + nbClientDansAct);
+            //System.out.println("Boucle : " + nbClients+2+nbClientDansAct);
+            for (int j = nbClients + 2; j < nbClients + 2 + nbClientDansAct; j++) {
+
+                //j est initié à la position dans le tableau posClients où se trouve le premier client de l'étape
+                //S'il n'y a aucun client dans l'étape alors j >= à la condition qui sort de la boucle
+                System.out.print(posClients[j] + " ");
+                g.allerA(posClients[j], monde.getSortie(), monde.getSortie().getIndiceEtape());
+
+            }
+            System.out.println(" ");
+            etapeActuel++;
+            System.out.println("\n");
+            notifierObservateurs();
+            mondeIG.notifierObservateurs();
+        } while (posClients[monde.getSortie().getIndiceEtape() * nbClients + 1] != nbClients && mondeIG.getEnCoursDeSim()); //while tous les clients ne sont pas dans le sasSortie donc posClient[nbAct-1 + nbClient * nbAct-1] == nbClient
 
         System.out.println("La simulation est terminée");
 
         //affichage des numéros de processus :
         System.out.println("Les clients :");
-        for(int i=0; i<nbClients;i++){
-            System.out.print(resSim[i]+ " ");
+        for (int i = 0; i < nbClients; i++) {
+            System.out.print(resSim[i] + " ");
             kit.detruireLesProcessus(resSim[i]);
         }
 
-        if(mondeIG.getEnCoursDeSim()) {
+        if (mondeIG.getEnCoursDeSim()) {
             //La simulation n'a pas été interrompu en cours de sim
             mondeIG.switchEtatSim();
         }
 
-            mondeIG.notifierObservateurs();
-            nettoyage();
-            FabriqueNumero.getInstance().reset();
+        long finSim = System.nanoTime();
+        long tempsNano = finSim - debutSim;
+        double tempsSec = tempsNano / 1000000000.0;
+        mondeIG.setTempsSim(tempsSec);
+
+        mondeIG.notifierObservateurs();
+        nettoyage();
+        FabriqueNumero.getInstance().reset();
 
         ThreadsManager.getInstance().detruireTout();
-
 
     }
 
